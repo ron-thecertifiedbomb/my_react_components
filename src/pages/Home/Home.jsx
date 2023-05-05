40443e7ffbef1e1bd84fb8a5a0b6181d7f4653c8
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-05-05 08:12:33 +0800

    placed accordion to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-import PaginationApp from "../../components/Pagination/Pagination";
-import SearchBar from "../../components/SearchBar/SearchBar";
+// import PaginationApp from "../../components/Pagination/Pagination";
+// import SearchBar from "../../components/SearchBar/SearchBar";
+import Accordion from "../../components/Accordion/Accordion";
 import "./Home.css";
 
 const Home = () => {
@@ -9,12 +10,15 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <div className="search_bar_wrapper">
+
+
+      <Accordion />
+      {/* <div className="search_bar_wrapper">
         <SearchBar onQuery ={SetQuery}/> 
       </div>
       <div className="pagination-container">
         <PaginationApp query={query}/>
-      </div>
+      </div> */}
 
       
     </div>
